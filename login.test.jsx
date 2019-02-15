396ac6e68ed8551d144ff086e386d11f923266d8
Marcin Kołodziej <36101983+kolodziejm@users.noreply.github.com>
2019-02-15 10:56:17 +0100

    Update login.test.jsx

--- login.test.jsx
+++ login.test.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import {renderIntoDocument, cleanup} from 'react-testing-library' // zamiennik enzyme
+import {render, cleanup} from 'react-testing-library' // zamiennik enzyme
 import {Login} from './login'
 
 afterEach(cleanup)
 
 test('calls onSubmit with username and password', () => {
-const handleSubmit = jest.fn()
-const {getByLabelText, getByText} = renderIntoDocument(
+const handleSubmit = jest.fn() // jest mock - poczytaj
+const {getByLabelText, getByText} = render( // render zwraca wszystkie selektory do wykorzystania
 <Login onSubmit={handleSubmit} />,
 )
-getByLabelText(/username/i).value = 'chuck'
+getByLabelText(/username/i).value = 'chuck' // podstawowy regexp
 getByLabelText(/password/i).value = 'norris'
 getByText(/submit/i).click()
-expect(handleSubmit).toHaveBeenCalledTimes(1)
+expect(handleSubmit).toHaveBeenCalledTimes(1) // assortions
 expect(handleSubmit).toHaveBeenCalledWith({
 username: 'chuck',
 password: 'norris',
